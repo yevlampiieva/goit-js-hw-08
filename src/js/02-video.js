b8091fcf6e47b35146d6134e47bae471f3efcead
Alla Yevlampiieva <modan1679@gmail.com>
2023-05-28 03:44:31 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+function getCurrentTime(data) {
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data.seconds));
+}
+
+const currentTime = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) ?? 0;
+
+player.setCurrentTime(currentTime);
